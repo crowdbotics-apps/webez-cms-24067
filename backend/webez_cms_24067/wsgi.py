0c8b49c7b4a9be33b67814ff0f3c09e73e4f08b8
Crowdbotics <team@crowdbotics.com>
2021-01-25 21:43:22 +0000

    Update models

--- backend/webez_cms_24067/wsgi.py
+++ backend/webez_cms_24067/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webez_cms_24067.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webez_cms_24067.settings")
 
 application = get_wsgi_application()
